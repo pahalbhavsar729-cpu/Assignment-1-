5506e58412522fa14f5758b8e9104d37c315f53f
pahalbhavsar729-cpu <pahalbhavsar729@gmail.com>
2025-09-28 19:33:35 +0530

    Add files via upload

---
+++ Assignment 1 Task 1 + Task 2.py
@@ -0,0 +1,24 @@
+#Task 1#
+#Question 1#
+num1 =  int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+print("The two numbers are : ",num1  ,  num2) 
+
+#Question 2#
+print("The Addition of these two numbers are :",num1 + num2)
+print("The Subtraction of these two numbers are :",num1 - num2)
+print("The Multiplication of these two numbers are :",num1 * num2)
+print("The Subtraction of these two numbers are :",num1 / num2)
+
+
+#Task 2#
+#Question 1#
+first_name = input("Enter the  First name of the user :")
+last_name = input("Enter the last name of the user :")
+
+#Question 2#
+full_name = first_name + last_name 
+
+#Question 3#
+print("Hello !",full_name, "Welcome to the Python Program")
+
